fa917fd405acf8b67b46c0ad8dca1f73c118230a
Nguyen Duc Cong <nguyenduccong2005@gmail.com>
2024-04-14 17:23:45 +0700

    Same with the lastest update but pt2.
    -Working on Attribute Component.

--- Adventure1/AttributeComponent.cpp
+++ Adventure1/AttributeComponent.cpp
@@ -1,9 +1,39 @@
 #include "stdafx.h"
 #include "AttributeComponent.h"
 
-AttributeComponent::AttributeComponent()
+AttributeComponent::AttributeComponent(unsigned level)
 {
+	this->level = level;
+	this->exp = 0;
+	this->expNext = 0;
+	this->attributePoint = 3;
+
+	this->vitality = 1;
+	this->strength = 1;
+	this->dexterity = 1;
+	this->intelligence = 1;
+	this->constitution = 1;
+	this->agility = 1;
+
+	this->UpdateStats();
 }
 AttributeComponent::~AttributeComponent()
+{
+}
+
+void AttributeComponent::CalulateExpNext()
+{
+}
+
+void AttributeComponent::UpdateStats()
+{
+
+}
+
+void AttributeComponent::LevelUp()
+{
+}
+
+void AttributeComponent::Update()
 {
 }
--- Adventure1/AttributeComponent.h
+++ Adventure1/AttributeComponent.h
@@ -5,7 +5,37 @@
 class AttributeComponent
 {
 public:
-	AttributeComponent();
+	//leveling
+	unsigned level;
+	unsigned exp;
+	unsigned expNext;
+	unsigned attributePoint;
+
+	//attributes
+	unsigned vitality; //max health
+	unsigned strength; //crush the tomato
+	unsigned dexterity;//dodge the tomato
+	unsigned intelligence;//knowing the tomato?
+	unsigned constitution; //eat the rotten tomato
+	unsigned agility; // run
+
+	//stats
+	int hp;
+	int hpMax;
+	int damageMin;
+	int damageMax;
+	int accuracy;
+	int dodge;
+	int luck;
+
+	//con & de
+	AttributeComponent(unsigned level);
 	~AttributeComponent();
+
+	//func
+	void CalulateExpNext();
+	void UpdateStats();
+	void LevelUp();
+	void Update();
 };
 
